49970693d76ad1b1202822e7fff7909bcf525ff5
Minghe Huang <h.minghe@gmail.com>
2019-10-17 18:27:21 +0800

    brute force

--- group-anagrams/index.test.js
+++ group-anagrams/index.test.js
@@ -4,7 +4,7 @@ const {
   usePrime,
 } = require('./index')
 
-test("group anagram", () => {
+test.skip("group anagram", () => {
   const input =  ["hos","boo","nay","deb","wow","bop","bob","brr","hey","rye","eve","elf","pup","bum","iva","lyx","yap","ugh","hem","rod","aha","nam","gap","yea","doc","pen","job","dis","max","oho","jed","lye","ram","pup","qua","ugh","mir","nap","deb","hog","let","gym","bye","lon","aft","eel","sol","jab"]
   const output = usePrime(input);
   console.log(JSON.stringify(output, null, 2))
