2f20f5fdd2c54f82caefb6460f2843b06e1fddad
Minghe Huang <h.minghe@gmail.com>
2019-10-22 12:50:44 +0800

    perf

--- letter-combinations-of-a-phone-number/index.js
+++ letter-combinations-of-a-phone-number/index.js
@@ -1,12 +1,12 @@
 const dict = {
-  '2': 'abc',
-  '3': 'def',
-  '4': 'ghi',
-  '5': 'jkl',
-  '6': 'mno',
-  '7': 'pqrs',
-  '8': 'tuv',
-  '9': 'wxyz',
+  '2': ['a', 'b','c'],
+  '3': ['d', 'e', 'f'],
+  '4': ['g', 'h', 'i' ],
+  '5': ['j', 'k', 'l'],
+  '6': ['m', 'n', 'o'],
+  '7': ['p', 'q', 'r', 's'],
+  '8': ['t', 'u', 'v'],
+  '9': ['w', 'x', 'y', 'z'],
 }
 const letterCombinations = (input) => {
   if (input === "") {
@@ -17,7 +17,7 @@ const letterCombinations = (input) => {
 };
 
 const combine = (digits, n) => {
-  const arr = dict[digits[n]].split('')
+  const arr = dict[digits[n]]
   if (n === 0) {
     return arr
   }
