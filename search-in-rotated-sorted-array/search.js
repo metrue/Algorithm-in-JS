16d3393e2c0a23a883e3e5b21b3e6bdc1324711f
Minghe Huang <h.minghe@gmail.com>
2019-11-01 15:36:36 +0800

    update

--- search-in-rotated-sorted-array/search.js
+++ search-in-rotated-sorted-array/search.js
@@ -8,26 +8,17 @@ const search = (nums, target) => {
       return mid
     }
 
-    // left is in order
-    if (nums[start] < nums[mid]) {
-      if (target >= nums[start] && target < nums[mid]) {
+    if (nums[start] <= nums[mid]) {
+      if (nums[start] <= target && target < nums[mid]) {
         end = mid - 1
       } else {
         start = mid + 1
       }
-    // right is in order
-    } else if (nums[mid] < nums[end]) {
-      if (target > nums[mid] && target <= nums[end]) {
+    } else if (nums[mid] <= nums[end]) {
+      if (nums[mid] < target && target <= nums[end]) {
         start = mid + 1
       } else {
         end = mid - 1
-      }
-    } else {
-      if (nums[start] === nums[mid]) {
-        start++
-      }
-      if (nums[end] === nums[mid]) {
-        end--
       }
     }
   }
